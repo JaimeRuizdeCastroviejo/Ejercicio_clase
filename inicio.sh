5973fc610dfc9a099662f06ad1ab4deb8a34cf2a
Jaime Ruiz de Castroviejo <jaimer.castroviejo@gmail.com>
2023-04-12 14:36:47 +0200

    Guardado 6

---
+++ inicio.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+clear
+
+# git init
+git config user.name "JaimeRuizdeCastroviejo"
+git config user.email "jaimer.castroviejo@gmail.com"
+git config user.password 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
+
+#git add .
+#git commit -m "FOTO 3"
+
+git remote add origin https://github.com/JaimeRuizdeCastroviejo/Ejercicio_clase.git
+git branch -M main
+git push -u origin main
